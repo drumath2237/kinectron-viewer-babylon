d2237fdb4d315a6c4988fae68c0f17e666acad4e
drumath <2237k.drumath@gmail.com>
2022-03-26 19:20:52 +0900

    :sparkles: テスト用のlib.tsを削除

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
-import Lib from './lib.ts';
-
 export default class App {
   /**
    * entry point
    */
   static main() {
-    const hello = new Lib('hello');
-    console.log(hello);
     // const kinectron = new Kinectron("127.0.0.1");
     // const rawDepthWidth = 640 / 2;
     // const rawDepthHeight = 576 / 2;
-
     // kinectron.setKinectType("azure");
     // kinectron.makeConnection();
-
     // kinectron.startRawDepth((depthBuffer) => {
     //   console.log(depthBuffer);
     // });
